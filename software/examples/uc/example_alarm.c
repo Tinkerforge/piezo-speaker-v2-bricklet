21b3ff7ca67f33ff19343152e052911fd809890e
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:43:22 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_alarm.c
+++ software/examples/uc/example_alarm.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_piezo_speaker_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_piezo_speaker_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Piezo Speaker Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_PiezoSpeakerV2 ps;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_piezo_speaker_v2_create(&ps, UID, hal), "create device object");
+	check(tf_piezo_speaker_v2_create(&ps, NULL, hal), "create device object");
 
 	// 10 seconds of loud annoying fast alarm
 	check(tf_piezo_speaker_v2_set_alarm(&ps, 800, 2000, 10, 1, 10,
